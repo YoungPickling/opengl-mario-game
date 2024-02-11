3b5883d5a6eaee5b67a5adeb9987408da8f9250f
Maximas <maksimas.pavlenko@gmail.com>
2024-02-11 10:29:13 +0200

    fit: shader glsl file

--- src/main/java/lt/mario/jade/LevelEditorScene.java
+++ src/main/java/lt/mario/jade/LevelEditorScene.java
@@ -2,9 +2,28 @@ package lt.mario.jade;
 
 public class LevelEditorScene extends Scene {
 
-//    private boolean changingScene;
-//
-//    private float timeToChangeScene = 2.0f;
+    private String vertexShaderSrc = "#version 330 core\n" +
+            "layout (location=0) in vec3 aPos;\n" +
+            "layout (location=1) in vec4 aColor;\n" +
+            "\n" +
+            "out vec4 fColor;\n" +
+            "\n" +
+            "void main()\n" +
+            "{\n" +
+            "    fColor = aColor;\n" +
+            "    gl_Position = vec4(aPos, 1.0);\n" +
+            "}";
+    private String fragmentShaderSrc = "#version 330 core\n" +
+            "\n" +
+            "in vec4 fColor;\n" +
+            "\n" +
+            "out vec4 color;\n" +
+            "\n" +
+            "void main()\n" +
+            "{\n" +
+            "    color = fColor;\n" +
+            "}";
+    private int vertexID, fragmentID, shaderProgram;
 
     public LevelEditorScene() {
 //        System.out.println("Inside level editor scene");
